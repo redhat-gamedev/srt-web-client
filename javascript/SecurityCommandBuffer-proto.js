09599ade1e89bb02ca5908319d49f75010c81bf9
Erik M Jacobs <erikmjacobs@gmail.com>
2021-01-04 15:36:19 -0500

    attempt to send join message

---
+++ javascript/SecurityCommandBuffer-proto.js
@@ -0,0 +1,8 @@
+var SecurityCommandBuffer_SecurityCommandBuffer
+protobuf.load("../proto/SecurityCommandBuffer.proto", function(err, root) {
+  if (err)
+    throw err;
+
+  // Obtain message type
+  SecurityCommandBuffer_SecurityCommandBuffer = root.lookupType("redhatgamedev.srt.SecurityCommandBuffer");
+});
