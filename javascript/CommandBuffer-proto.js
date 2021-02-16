496258e2d2dd874c15686bc96a660afd67f6f707
Jared Sprague <jared@caramelcode.com>
2021-02-15 20:00:54 -0500

    Adding more file structure and TypeScript configs

--- javascript/CommandBuffer-proto.js
+++
@@ -1,9 +0,0 @@
-var CommandBuffer
-protobuf.load("../proto/CommandBuffer.proto", function(err, root) {
-  if (err)
-    throw err;
-  
-  // Obtain a message type
-  CommandBuffer = root.lookupType("redhatgamedev.srt.CommandBuffer");
-
-});
