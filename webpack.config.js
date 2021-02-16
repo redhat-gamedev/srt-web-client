6fb721f96b5e0fe267267bd35479a8ae6be3bcd4
Jared Sprague <jared@caramelcode.com>
2021-02-16 12:29:12 -0500

    Upgraded to latest version of Phaser

--- webpack.config.js
+++ webpack.config.js
@@ -4,37 +4,37 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.ts',
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
+    mode  : 'development',
+    entry : './src/index.ts',
+    module: {
+        rules: [
+            {
+                test   : /\.tsx?$/,
+                use    : 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        filename: 'bundle.js',
+        path    : path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'src/index.html',
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: 'src/assets', to: 'assets' },
+            ],
+        }),
+        new BrowserSyncPlugin({
+            host  : 'localhost',
+            port  : 3000,
+            server: { baseDir: ['dist'] },
+        }),
     ],
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  },
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'src/index.html'
-    }),
-    new CopyWebpackPlugin({
-      patterns: [
-        { from: "src/assets", to: "assets" },
-      ],
-    }),
-    new BrowserSyncPlugin({
-      host: 'localhost',
-      port: 3000,
-      server: { baseDir: ['dist'] }
-    })
-  ],
 };
